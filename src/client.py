dc79a630b3b07017f4cb1c2216f1a8fa049fbd6d
akapkotel <btcuserbtc@gmail.com>
2021-10-19 16:20:01 +0200

    Added .gitignore, created dist for linux, mooved source files to src directory.

---
+++ src/client.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+from typing import List, Tuple
+
+import arcade
+
+from arcade import is_point_in_polygon
+from arcade.key import LSHIFT, W, S, A, D
+from game import Player, Projectile, Map, PLAYERS_COLORS
+from networking import NetworkClient
+from visibility import VisibleArea
+
+WIDTH = 500
+HEIGHT = 500
+TITLE = 'Client'
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
+class GameClientWindow(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        self.network_client = NetworkClient()
+        self.game_view = None
+        self.menu_view = MenuView()
+        self.show_view(self.menu_view)
+
+    def on_close(self):
+        print('CLosing and disconnecting from sever...')
+        if self.game_view is not None:
+            self.game_view.local_player.kill()
+        self.network_client.disconnect(self.game_view.local_player)
+        super().on_close()
+
+
+class MenuView(arcade.View):
+
+    def on_draw(self):
+        super().on_draw()
+        self.window.clear(color=WHITE)
+        arcade.draw_text('Menu', 250, 250, color=BLACK)
+
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        self.window.game_view = game = GameView()
+        self.window.show_view(game)
+
+
+class GameView(arcade.View):
+    
+    def __init__(self):
+        super().__init__()
+        self.mouse_position = [0, 0]
+        self.local_player = None
+        self.enemy_player = None
+        self.players = {}
+        self.projectiles = set()
+        self.map = Map()
+        self.visible_area = VisibleArea()
+        self.keys_pressed = set()
+        self.setup_players()
+    
+    @property
+    def all_players_in_game(self) -> bool:
+        return all(p.active for p in self.players.values())
+
+    def setup_players(self):
+        self.local_player = local_player = self.window.network_client.connect()
+        game_id = local_player.game_id
+
+        for i in range(4):
+            if i == local_player.id:
+                self.players[i] = self.local_player
+            else:
+                self.players[i] = Player(game_id, i, 250, 250, 25, 35, PLAYERS_COLORS[i], active=i <= local_player.id)
+
+    def on_draw(self):
+        super().on_draw()
+        self.window.clear(color=BLACK)
+        self.draw_game_objects()
+        arcade.draw_text(str(f'Health: {self.local_player.health}'), 250, 20, WHITE)
+
+    def draw_game_objects(self):
+        for obstacle in self.map.obstacles:
+            arcade.draw_polygon_filled(obstacle.vertices, WHITE)
+        for player in (p for p in self.players.values() if p.alive and self.is_object_visible(p)):
+            player.draw()
+        for projectile in self.projectiles:
+            projectile.draw()
+
+    def update(self, delta_time: float):
+        super().update(delta_time)
+        if self.local_player.is_moving:
+            self.update_visible_area()
+        if self.all_players_in_game:
+            self.update_players()
+            self.update_projectiles()
+            self.process_keyboard_input()
+            self.local_player.aim_at_the_cursor_position(*self.mouse_position)
+        if self.local_player.active:
+            self.share_data_with_server()
+
+    def update_players(self):
+        for player in list(p for p in self.players.values()):
+            if player.alive:
+                player.update(player == self.local_player)
+            else:
+                del self.players[player.id]
+
+    def update_projectiles(self):
+        for projectile in self.projectiles.copy():
+            if projectile.active:
+                projectile.update()
+                self.check_for_collisions(projectile)
+            else:
+                self.projectiles.remove(projectile)
+
+    def process_keyboard_input(self):
+        player = self.local_player
+        speed = player.speed
+        player.stop()
+        if LSHIFT in self.keys_pressed:
+            speed *= 1.5
+        if W in self.keys_pressed:
+            player.forward(speed)
+        if S in self.keys_pressed:
+            player.reverse(speed)
+        if A in self.keys_pressed:
+            player.rotate(1)
+        if D in self.keys_pressed:
+            player.rotate(-1)
+
+    def share_data_with_server(self):
+        enemies, projectiles = self.window.network_client.send(self.local_player)
+        if enemies is not None:
+            self.players.update({enemy.id: enemy for enemy in enemies})
+        if projectiles is not None:
+            self.projectiles.update(projectiles)
+
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        if (projectile := self.local_player.shoot(x, y)) is not None:
+            self.projectiles.add(projectile)
+            self.window.network_client.send(projectile)
+
+    def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
+        self.mouse_position = x, y
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        self.keys_pressed.add(symbol)
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        self.keys_pressed.discard(symbol)
+
+    def is_object_visible(self, p) -> bool:
+        return p is self.local_player or p in self.visible_area
+
+    def update_visible_area(self):
+        position = self.local_player.position
+        self.update_viewport(*position)
+        visible_map_rect = self.get_viewport_rect()
+        self.map.update_visible_map_area(visible_map_rect)
+        visible_obstacles = self.map.get_visible_obstacles()
+        self.visible_area.update(position, visible_map_rect, visible_obstacles)
+
+    def update_viewport(self, x, y):
+        # TODO
+        pass
+
+    def get_viewport_rect(self) -> List[Tuple]:
+        x, y, w, h = self.window.viewport
+        return [(x, y), (x + w, y), (x + w, y + h), (x, y + h)]
+
+    def check_for_collisions(self, projectile: Projectile):
+        x, y = projectile.position
+        self.check_for_collisions_with_obstacles(projectile, x, y)
+        self.check_for_collisions_with_players(projectile, x, y)
+
+    def check_for_collisions_with_players(self, projectile, x, y):
+        for player in self.players.values():
+            if player.alive and is_point_in_polygon(x, y, player.polygon):
+                projectile.kill()
+                if player is self.local_player:
+                    self.local_player.damage(projectile)
+
+    def check_for_collisions_with_obstacles(self, projectile, x, y):
+        for obstacle in self.map.get_visible_obstacles():
+            if is_point_in_polygon(x, y, obstacle):
+                if obstacle.destructible:
+                    obstacle.damage(x, y)
+                projectile.kill()
+
+
+if __name__ == '__main__':
+    client = GameClientWindow(WIDTH, HEIGHT, TITLE)
+    arcade.run()
---
+++ src/server.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+import socket
+import time
+import threading
+
+from typing import List, Optional
+from pickle import dumps, loads
+from game import Game, Player, Projectile
+
+
+def log(message: str, console: bool = False):
+    logged_message = f'{time.asctime()}, {message}'
+    if console:
+        print(logged_message)
+    with open('../logs.txt', 'a') as log_file:
+        print(logged_message, file=log_file, flush=True)
+
+
+def clear_log_file():
+    with open('../logs.txt', 'w') as file:
+        file.truncate(0)
+
+
+class Server:
+    def __init__(self):
+        self.games: List[Game] = []
+        self.server_ip_address = socket.gethostbyname(socket.gethostname())
+        self.port = 5555
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        if self.bind_socket():
+            self.run_server()
+
+    def bind_socket(self):
+        try:
+            self.socket.bind((self.server_ip_address, self.port))
+            return True
+        except socket.error as e:
+            log(e)
+            return False
+
+    def run_server(self):
+        self.socket.listen(4)
+        log('Server started, waiting for the connections.', console=True)
+        while True:
+            try:
+                connection, address = self.socket.accept()
+                thread = threading.Thread(target=self.threaded_client, args=(connection, address[0]), daemon=False)
+                thread.start()
+            except KeyboardInterrupt:
+                break
+        self.socket.close()
+
+    def threaded_client(self, connection: socket, address: str):
+        log(f'Received connection from: {address}')
+
+        game_request = loads(connection.recv(128))
+        game_name, max_players = game_request['game_name'], game_request['max_players']
+
+        game = self.add_client_to_game(address, game_name, max_players)
+        self.send_client_response_with_game_and_player_id(connection, game)
+
+        self.play_game_until_disconnected_or_dead(address, connection, game)
+
+        if game in self.games and not game.players:
+            self.games.remove(game)
+
+        log(f'Disconnected with {address}')
+
+        connection.close()
+
+    def play_game_until_disconnected_or_dead(self, address, connection, game):
+        while True:
+            try:
+                if received := loads(connection.recv(2048)):
+                    log(f'Game: {game.id}, received data: {received} from {address}')
+                    self.process_and_response(game, received, connection)
+                else:
+                    break
+            except (EOFError, ConnectionError) as e:
+                log(str(e))
+                break
+
+    def add_client_to_game(self, client_ip_address, game_name=None, max_players=4) -> Game:
+        game = self.get_game_instance(game_name, max_players)
+        game.join_new_player(client_ip_address)
+        return game
+
+    def get_game_instance(self, game_name, max_players) -> Game:
+        if game_name is not None:
+            return self.get_private_game(game_name, max_players)
+        else:
+            return self.get_public_game(max_players)
+
+    def get_private_game(self, game_name, max_players):
+        for game in (g for g in self.games if g.name == game_name):
+            return game
+        return self.create_new_game(game_name, max_players)
+
+    def get_public_game(self, max_players):
+        for game in (g for g in self.games if g.public):
+            if game.can_player_join():
+                return game
+        return self.create_new_game(max_players)
+
+    def create_new_game(self, game_name: str = None, max_players: int = 4) -> Game:
+        new_game = Game(game_id=len(self.games), name=game_name, max_players=max_players)
+        self.games.append(new_game)
+        return new_game
+
+    def send_client_response_with_game_and_player_id(self, connection: socket, game: Game):
+        connection.send(dumps(game.last_added_player()))
+
+    def process_and_response(self, game: Game, received: Player or Projectile, connection: socket):
+        if isinstance(received, Player):
+            game.update_player(received)
+
+            if game.players:
+                other_players, projectiles = game.get_other_players_and_projectiles(received)
+                connection.sendall(dumps((other_players, projectiles)))
+            else:
+                connection.sendall(str.encode('w'))
+        elif isinstance(received, Projectile):
+            game.update_projectiles(received)
+
+
+if __name__ == '__main__':
+    clear_log_file()
+    server = Server()
